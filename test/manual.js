b1be148208589209aa6cb7f507400b3eab228c5f
Shaun Smith <darscan@gmail.com>
2018-09-07 11:06:53 +0100

    chore: fixup manual test script

--- test/manual.js
+++ test/manual.js
@@ -1,9 +1,19 @@
-var plugin = require('../lib');
+#!/usr/bin/env node
+
+var plugin = require('../dist');
+var analyzer = require('../dist/analyzer');
+
+function analyze(targetImage) {
+  return analyzer.analyze(targetImage);
+}
+
 
 function main() {
   var targetImage = process.argv[2];
+  var analyzeOnly = process.argv.indexOf('--analyze') > 0;
+  var cmd = analyzeOnly ? analyze : plugin.inspect;
 
-  plugin.inspect(targetImage).then(function (result) {
+  cmd(targetImage).then(function (result) {
     console.log(JSON.stringify(result, null, 2));
   }).catch(function (error) {
     console.log('Error:', error.stack);
