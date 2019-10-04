8b21442ff257a9db5a2599185c71f6c675d99c24
ivanstanev <istanev93@gmail.com>
2019-10-04 12:34:18 +0100

    chore: add return type to getRuntime()

--- lib/inputs/runtime/docker.ts
+++ lib/inputs/runtime/docker.ts
@@ -1,6 +1,8 @@
 import { Docker, DockerOptions } from "../../docker";
 
-export function getRuntime(options: DockerOptions) {
+export function getRuntime(
+  options: DockerOptions,
+): Promise<string | undefined> {
   return Docker.run(["version"], options)
     .then((output) => {
       const versionMatch = /Version:\s+(.*)\n/.exec(output.stdout);
