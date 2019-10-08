694352df508f119fd4137f92e89e0f4abb208b7e
ivanstanev <istanev93@gmail.com>
2019-10-08 17:26:56 +0100

    test: fix stream-utils test

--- test/lib/stream-utils.test.ts
+++ test/lib/stream-utils.test.ts
@@ -3,21 +3,21 @@
 // See: https://github.com/tapjs/node-tap/issues/313#issuecomment-250067741
 
 import { createReadStream } from "fs";
+import { join } from "path";
 import { test } from "tap";
 import { streamToBuffer, streamToString } from "../../lib/stream-utils";
 
+const getFixture = (fixturePath) =>
+  join(__dirname, "../fixtures/generic", fixturePath);
+
 test("stream-utils.streamToString()", async (t) => {
-  const fileStream = createReadStream(
-    "../fixtures/generic/small-sample-text.txt",
-  );
+  const fileStream = createReadStream(getFixture("small-sample-text.txt"));
   const fileContent = await streamToString(fileStream);
   t.same(fileContent, "Hello, world!");
 });
 
 test("stream-utils.streamToBuffer()", async (t) => {
-  const fileStream = createReadStream(
-    "../fixtures/generic/small-sample-text.txt",
-  );
+  const fileStream = createReadStream(getFixture("small-sample-text.txt"));
   const fileContent = await streamToBuffer(fileStream);
   t.deepEqual(
     fileContent,
