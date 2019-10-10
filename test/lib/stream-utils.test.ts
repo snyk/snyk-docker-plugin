01491aa1ba7d6ec7d3a6929186ed93d32f6ce041
ivanstanev <istanev93@gmail.com>
2019-10-10 14:58:47 +0100

    test: improve stream-utils test

--- test/lib/stream-utils.test.ts
+++ test/lib/stream-utils.test.ts
@@ -3,6 +3,7 @@
 // See: https://github.com/tapjs/node-tap/issues/313#issuecomment-250067741
 
 import { createReadStream } from "fs";
+import { readFileSync } from "fs";
 import { join } from "path";
 import { test } from "tap";
 import { streamToBuffer, streamToString } from "../../lib/stream-utils";
@@ -11,17 +12,25 @@ const getFixture = (fixturePath) =>
   join(__dirname, "../fixtures/generic", fixturePath);
 
 test("stream-utils.streamToString()", async (t) => {
-  const fileStream = createReadStream(getFixture("small-sample-text.txt"));
+  const fixture = getFixture("small-sample-text.txt");
+  const fileStream = createReadStream(fixture);
+
   const fileContent = await streamToString(fileStream);
-  t.same(fileContent, "Hello, world!");
+  const expectedContent = readFileSync(fixture, { encoding: "utf-8" });
+
+  t.same(fileContent, expectedContent, "Returned the expected string");
 });
 
 test("stream-utils.streamToBuffer()", async (t) => {
-  const fileStream = createReadStream(getFixture("small-sample-text.txt"));
+  const fixture = getFixture("small-sample-text.txt");
+  const fileStream = createReadStream(fixture);
+
   const fileContent = await streamToBuffer(fileStream);
+  const expectedContent = readFileSync(fixture);
+
   t.deepEqual(
     fileContent,
-    Buffer.from("Hello, world!"),
+    expectedContent,
     "streamToBuffer returns the expected content",
   );
 });
