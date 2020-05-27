7fa55991639cdd6a067c54ccd2851e19c372c7e4
Shai Mendel <shaimendel@gmail.com>
2020-05-27 17:32:53 +0300

    chore: change successful slack message content

--- .circleci/slack-notify-success.sh
+++ .circleci/slack-notify-success.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-curl -X POST -H 'Content-Type:application/json' -d '{"attachments": [{"color": "#7CD197", "fallback": "Build Notification: '$CIRCLE_BUILD_URL'", "title": "Snyk-Docker-Plugin Publish Notification", "text": ":krotik-yay: Snyk-Docker-Plugin Was Published :krotik-yay:"}]}' $SLACK_WEBHOOK
+curl -X POST -H 'Content-Type:application/json' -d '{"attachments": [{"color": "#7CD197", "fallback": "Build Notification: '$CIRCLE_BUILD_URL'", "title": "Snyk-Docker-Plugin Build Notification", "text": ":krotik-yay: Snyk-Docker-Plugin Successful Master Build :krotik-yay:"}]}' $SLACK_WEBHOOK
