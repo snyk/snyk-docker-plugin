dd2de72aa4c3c2fb7293fcb3a86cfa40d46b0db2
agatakrajewska <agata.krajewska@snyk.io>
2020-11-17 17:17:49 +0000

    chore: Moving app scan tests to jest

---
+++ test/windows/application-scans.spec.ts
@@ -0,0 +1,20 @@
+import { join as pathJoin } from "path";
+import { scan } from "../../lib/index";
+
+function getFixture(fixturePath: string): string {
+  return pathJoin(__dirname, "../fixtures/docker-archives", fixturePath);
+}
+
+describe("scanning a container image with 2 applications", () => {
+  it("should return expected result", async () => {
+    const fixturePath = getFixture("skopeo-copy/rpm-npm-yarn.tar");
+    const imageNameAndTag = `docker-archive:${fixturePath}`;
+
+    const pluginResult = await scan({
+      path: imageNameAndTag,
+      "app-vulns": true,
+    });
+
+    expect(pluginResult).toMatchSnapshot();
+  });
+});
