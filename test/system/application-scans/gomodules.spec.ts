4b2b1427e75102990d8596984373730afef59d45
Ivan Stanev <istanev93@gmail.com>
2021-02-09 12:18:11 +0200

    test: show that errors in go parsing throw an uncaught exception

--- test/system/application-scans/gomodules.spec.ts
+++ test/system/application-scans/gomodules.spec.ts
@@ -1,7 +1,13 @@
-import { scan } from "../../../lib";
+import * as elf from "elfy";
+
+import { PluginResponse, scan } from "../../../lib";
 import { getFixture } from "../../util";
 
 describe("gomodules binaries scanning", () => {
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
   it("should return expected result", async () => {
     // Arrange
     const fixturePath = getFixture("docker-archives/docker-save/yq.tar");
@@ -15,5 +21,29 @@ describe("gomodules binaries scanning", () => {
 
     // Assert
     expect(pluginResult).toMatchSnapshot();
+  });
+
+  it("throws an uncaught exception when a binary cannot be parsed", async () => {
+    const elfParseMock = jest.spyOn(elf, "parse").mockImplementation(() => {
+      throw new Error("Cannot read property 'type' of undefined");
+    });
+
+    const fixturePath = getFixture("docker-archives/docker-save/yq.tar");
+    const imageNameAndTag = `docker-archive:${fixturePath}`;
+
+    try {
+      const pluginResult = await scan({
+        path: imageNameAndTag,
+        "app-vulns": true,
+      });
+      expect(pluginResult).toEqual<PluginResponse>({
+        scanResults: expect.any(Array),
+      });
+    } catch (error) {
+      // This won't be executed!
+      expect(error).toBeDefined();
+    }
+
+    elfParseMock.mockRestore();
   });
 });
