8f261561c6b96783e4f12ea12404164b7621bb98
Rotem <rotem.sagi@snyk.io>
2021-05-03 14:54:56 +0300

    fix: ignore gradle/caches path when extracting jars

--- lib/inputs/java/static.ts
+++ lib/inputs/java/static.ts
@@ -2,13 +2,15 @@ import * as path from "path";
 import { ExtractAction } from "../../extractor/types";
 import { streamToSha1 } from "../../stream-utils";
 
-const ignoredPaths = [path.normalize("/usr/lib")];
+const ignoredPaths = ["/usr/lib", "gradle/cache"];
 
 function filePathMatches(filePath: string): boolean {
   const dirName = path.dirname(filePath);
   return (
     filePath.endsWith(".jar") &&
-    !ignoredPaths.some((ignorePath) => dirName.startsWith(ignorePath))
+    !ignoredPaths.some((ignorePath) =>
+      dirName.includes(path.normalize(ignorePath)),
+    )
   );
 }
 
