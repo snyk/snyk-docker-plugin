d84cff28c7b9c2042349f69d4a8e297c57d026ae
Ivan Stanev <istanev93@gmail.com>
2022-01-18 18:41:38 +0000

    test: demonstrate that some OCI archives cannot be scanned

--- test/system/image-type/oci-archive.spec.ts
+++ test/system/image-type/oci-archive.spec.ts
@@ -12,6 +12,17 @@ describe("oci archive scanning", () => {
 
     expect(pluginResult).toMatchSnapshot();
   });
+
+  it("does not scan a busybox oci archive", async () => {
+    const fixturePath = getFixture("oci-archives/busybox-1.31.1.tar");
+    const imageNameAndTag = `oci-archive:${fixturePath}`;
+
+    await expect(async () =>
+      scan({
+        path: imageNameAndTag,
+      }),
+    ).rejects.toThrow(new Error("Invalid OCI archive"));
+  });
 });
 
 describe("handles bad input being provided", () => {
