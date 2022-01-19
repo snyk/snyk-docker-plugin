313d930ec924573616ff8b3b321caec8590fe10a
Shani Raby <shani.raby@gmail.com>
2022-01-19 16:24:25 +0200

    chore: remove local debugging harness

--- lib/index.ts
+++ lib/index.ts
@@ -44,36 +44,3 @@ export {
   Binary,
 };
 
-// tslint:disable:no-console
-setImmediate(async () => {
-  try {
-    const result = await scan({
-      // path:
-      // ""
-      // "docker-archive:/Users/shani/test-containers/buster-log4j/buster-log4j.tar",
-      // Options for "path" include, but not limited to:
-      // #1 [docker/oci]-arcive:/full/path/to/image.tar
-      // "docker-archive:/Users/shani/Downloads/dummy.tar",
-      // #2 public image
-      // "alpine",
-      // #3 remote registry (but then you'd also need username and password)
-      // "shanihub/my-image:latest",
-      // username: 'shanihub',
-      // password: 's3cr3t',
-      // #4 local registry
-      // other options, same as flags that you'd enter if you were running the CLI
-      // "app-vulns": true,
-      // "shaded-jars-depth": "2",
-      //  "file": "/Users/shani/test-containers/buster-log4j/Dockerfile"
-      machine: "true",
-      // ...
-    });
-
-    const depGraph = result.scanResults[0].facts.find(
-      (fact) => fact.type === "depGraph",
-    )?.data;
-    console.log(depGraph);
-  } catch (error) {
-    console.log(error);
-  }
-});
