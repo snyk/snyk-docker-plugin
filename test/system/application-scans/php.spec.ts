05341463525b911d1bf2ddc2231a95ca59fee6a0
Yaron Schwimmer <yaron.schwimmer@snyk.io>
2022-04-27 11:42:37 +0300

    feat: not returning PHP manifest files in response
    Since we are now analyzing PHP maniefst files in the plugin, returning
    the manifest files as well may cause a duplication in the scan results
    shown to users who are importing from the UI.
    Until we remove the relevant globs from the caller, this temporary
    solution removes the globs on the plugin side.

--- test/system/application-scans/php.spec.ts
+++ test/system/application-scans/php.spec.ts
@@ -15,6 +15,32 @@ describe("php application scans", () => {
     expect(pluginResult.scanResults).toHaveLength(2);
   });
 
+  it("should not return PHP manifest files", async () => {
+    const fixturePath = getFixture("docker-archives/docker-save/php.tar");
+    const imageNameAndTag = `docker-archive:${fixturePath}`;
+
+    const pluginResult = await scan({
+      path: imageNameAndTag,
+      "app-vulns": true,
+      globsToFind: {
+        include: [
+          "**/composer.json",
+          "**/composer.lock",
+          "**/requirements.txt",
+        ],
+        exclude: [],
+      },
+    });
+
+    expect(pluginResult).toMatchSnapshot();
+    expect(pluginResult.scanResults).toHaveLength(2);
+    expect(pluginResult.scanResults[0].facts).toHaveLength(6);
+    const factTypes = pluginResult.scanResults[0].facts.map(
+      (fact) => fact.type,
+    );
+    expect(factTypes).not.toContain("imageManifestFiles");
+  });
+
   it("should handle --app-vulns with string and boolean value", async () => {
     const fixturePath = getFixture("docker-archives/docker-save/php.tar");
     const imageNameAndTag = `docker-archive:${fixturePath}`;
