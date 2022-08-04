1147b143ab9477800fa29ff8157ed0ca77a9e9db
Yaron Schwimmer <yaron.schwimmer@snyk.io>
2022-08-04 15:55:52 +0300

    feat: scan poetry files in images
    Given a `pyproject.toml` manifest file and a `poetry.lock` file, both in
    the same directory, we will scan for application dependencies in the
    project they represent.

--- lib/analyzer/applications/index.ts
+++ lib/analyzer/applications/index.ts
@@ -1,4 +1,9 @@
 import { nodeFilesToScannedProjects } from "./node";
 import { phpFilesToScannedProjects } from "./php";
+import { poetryFilesToScannedProjects } from "./python";
 
-export { nodeFilesToScannedProjects, phpFilesToScannedProjects };
+export {
+  nodeFilesToScannedProjects,
+  phpFilesToScannedProjects,
+  poetryFilesToScannedProjects,
+};
