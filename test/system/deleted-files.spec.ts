8e847f85484f6e92c7cbe07d864df7ae438fb992
Steve <steve.shani@snyk.io>
2024-02-14 11:06:05 +0200

    fix: add support for deleted folders
    When a file or folder is deleted, an entry that starts with .wh is created.
    Up until now, Snyk detected files that were deleted, but not folders.
    
    This change adds support for deleted folders by checking whether a file path starts with the path of the deleted folder. Meaning if we know that "/app/dependencies" was deleted, then a file path like "/app/dependencies/jar/library.jar" will be marked as deleted as well.

--- test/system/deleted-files.spec.ts
+++ test/system/deleted-files.spec.ts
@@ -65,3 +65,78 @@ describe("deleted files", () => {
     ).toEqual(1);
   });
 });
+
+describe("Deleted Folders", () => {
+  const getFixture = (fixturePath) =>
+    path.join(__dirname, "../fixtures/docker-archives", fixturePath);
+
+  /*
+    The image in the example below was built with the following Dockerfile:
+
+    FROM alpine:latest
+    WORKDIR /app
+    RUN mkdir jar_folder && wget https://repo1.maven.org/maven2/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar && mv commons-collections-3.2.1.jar jar_folder
+    RUN rm -rf /app/jar_folder
+    RUN echo "hello"
+  */
+  it("commons-collections should be excluded from dependency tree", async () => {
+    const fixturePath = getFixture("docker-save/deleted-folder.tar");
+    const imagePath = `docker-archive:${fixturePath}`;
+
+    const pluginResult = await plugin.scan({
+      path: imagePath,
+    });
+
+    // There should only be the distro scan result. The maven scan result should not exist.
+    expect(pluginResult.scanResults.length).toEqual(1);
+  });
+
+  /*
+    The image in the example below was built with the following Dockerfile:
+
+    FROM alpine:latest
+    WORKDIR /app
+    RUN mkdir jar_folder &&  wget https://repo1.maven.org/maven2/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar && wget https://repo1.maven.org/maven2/com/google/guava/guava/30.1-jre/guava-30.1-jre.jar && mv commons-collections-3.2.1.jar guava-30.1-jre.jar jar_folder
+    RUN rm -rf /app/jar_folder
+    RUN echo "hello"
+   */
+  it("Both commons-collections and guava should be excluded from dependency tree", async () => {
+    const fixturePath = getFixture("docker-save/deleted-multi.tar");
+    const imagePath = `docker-archive:${fixturePath}`;
+
+    const pluginResult = await plugin.scan({
+      path: imagePath,
+    });
+
+    // There should only be the distro scan result. The maven scan result should not exist.
+    expect(pluginResult.scanResults.length).toEqual(1);
+  });
+
+  /*
+    The image in the example below was built with the following Dockerfile:
+
+    FROM alpine:latest
+    WORKDIR /app
+    RUN mkdir jar_folder &&  wget https://repo1.maven.org/maven2/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar && wget https://repo1.maven.org/maven2/com/google/guava/guava/30.1-jre/guava-30.1-jre.jar && mv commons-collections-3.2.1.jar guava-30.1-jre.jar jar_folder
+    RUN rm -rf /app/jar_folder
+    RUN mkdir jar_folder && wget https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar && mv commons-lang3-3.12.0.jar jar_folder
+    RUN echo "hello"
+
+   */
+  it("commons-lang3 should still exist even though the parent folder was deleted and recreated", async () => {
+    const fixturePath = getFixture("docker-save/deleted-recreated.tar");
+    const imagePath = `docker-archive:${fixturePath}`;
+
+    const pluginResult = await plugin.scan({
+      path: imagePath,
+    });
+
+    // There should only be the distro scan result. The maven scan result should not exist.
+    expect(pluginResult.scanResults.length).toEqual(2);
+
+    const result = pluginResult.scanResults[1];
+    expect(result.facts["0"].data.fingerprints["0"].artifactId).toEqual(
+      "commons-lang3",
+    );
+  });
+});
