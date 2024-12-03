5b13b8db3ddb4da9a40d7836202ea2f3cc2d24b3
David Gonoradsky <davidgonor123@gmail.com>
2024-12-03 16:47:02 +0200

    fix: remove duplicate interface

--- lib/analyzer/applications/types.ts
+++ lib/analyzer/applications/types.ts
@@ -43,13 +43,3 @@ export interface ApplicationFiles {
 }
 
 export type FilesByDirMap = Map<string, Set<string>>;
-
-export interface ApplicationFileInfo {
-  path: string;
-}
-export interface ApplicationFiles {
-  fileHierarchy: ApplicationFileInfo[];
-  moduleName?: string;
-  jarPath?: string;
-  language: string;
-}
