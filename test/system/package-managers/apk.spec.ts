652480688791c590638d6179ed68e0a4e48758a5
Brian Gardiner <brian.gardiner@snyk.io>
2025-06-18 14:38:03 -0400

    feat: add support for new wolfi/chainguard apk database location CN-150

--- test/system/package-managers/apk.spec.ts
+++ test/system/package-managers/apk.spec.ts
@@ -3,13 +3,29 @@ import { execute } from "../../../lib/sub-process";
 
 describe("apk package manager tests", () => {
   afterAll(async () => {
-    await execute("docker", ["image", "rm", "alpine:3.12.0"]).catch(() => {
+    await execute("docker", [
+      "image",
+      "rm",
+      "alpine:3.12.0",
+      "chainguard/bash@sha256:642933df66209814502599053ca3dfa97cccf847badc4219d2b1fd6565f6559a",
+    ]).catch(() => {
       console.error(`tests teardown failed to remove docker image`);
     });
   });
 
   it("should correctly analyze an apk image", async () => {
     const image = "alpine:3.12.0";
+    const pluginResult = await scan({
+      path: image,
+      platform: "linux/amd64",
+    });
+
+    expect(pluginResult).toMatchSnapshot();
+  });
+
+  it("should correctly analyze an apk image with newer wolfi/chainguard database location at /usr/lib/apk/", async () => {
+    const image =
+      "chainguard/bash@sha256:642933df66209814502599053ca3dfa97cccf847badc4219d2b1fd6565f6559a";
     const pluginResult = await scan({
       path: image,
       platform: "linux/amd64",
