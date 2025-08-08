cb143175266262ffd481e011c44081e71791fbe9
Parker Kuivila <parker.kuivila@snyk.io>
2025-08-08 14:12:08 -0400

    chore: unit tests

--- test/lib/extractor/index.spec.ts
+++ test/lib/extractor/index.spec.ts
@@ -1,5 +1,6 @@
 import { getContentAsString } from "../../../lib/extractor";
 import { ExtractAction, ExtractedLayers } from "../../../lib/extractor/types";
+import { isWhitedOutFile } from "../../../lib/extractor";
 
 describe("index", () => {
   test("getContentAsString() does matches when a pattern is used in the extract action", async () => {
@@ -16,5 +17,41 @@ describe("index", () => {
 
     //  extracted string matches
     expect(result).toEqual("Hello, world!");
+  });
+});
+
+describe("isWhitedOutFile", () => {
+  test("should return true for files containing .wh. in their path", () => {
+    expect(isWhitedOutFile("/etc/.wh.hosts")).toBe(true);
+    expect(isWhitedOutFile("/var/lib/.wh.data")).toBe(true);
+    expect(isWhitedOutFile("/.wh.config")).toBe(true);
+  });
+
+  test("should return false for files not containing .wh.", () => {
+    expect(isWhitedOutFile("/etc/hosts")).toBe(false);
+    expect(isWhitedOutFile("")).toBe(false);
+    expect(isWhitedOutFile("/")).toBe(false);
+  });
+
+  test("should return false for similar but different patterns", () => {
+    // make sure the dots are literal and not match all
+    expect(isWhitedOutFile("/etc/wh.hosts")).toBe(false);
+    expect(isWhitedOutFile("/etc/.whosts")).toBe(false);
+    expect(isWhitedOutFile("/etc/whhosts")).toBe(false);
+    
+    // dots in wrong places
+    expect(isWhitedOutFile("/etc/.w.h.hosts")).toBe(false);
+    expect(isWhitedOutFile("/etc/..wh..hosts")).toBe(false);
+    
+    // case sensitive
+    expect(isWhitedOutFile("/etc/.WH.hosts")).toBe(false);
+    expect(isWhitedOutFile("/etc/.Wh.hosts")).toBe(false);
+  });
+
+  test("should handle .wh. at different positions", () => {
+    expect(isWhitedOutFile(".wh.start")).toBe(true);
+    expect(isWhitedOutFile("middle.wh.file")).toBe(true);
+    expect(isWhitedOutFile("end.wh.")).toBe(true);
+    expect(isWhitedOutFile("/deeply/nested/path/.wh.present")).toBe(true);
   });
 });
