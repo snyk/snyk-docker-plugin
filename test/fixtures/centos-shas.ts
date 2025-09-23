0fc875b4d211058e5439cff9a624aa0cdb0d2e5d
Parker Kuivila <parker.kuivila@snyk.io>
2025-09-23 15:11:12 -0400

    fix: make linter happy

--- test/fixtures/centos-shas.ts
+++ test/fixtures/centos-shas.ts
@@ -3,6 +3,8 @@
 // Run `npm run update-quay-tests` to fetch the latest SHA digests
 
 export const CENTOS_SHAS = {
-  stream9: "sha256:0cae7e53f997228f0afc523f1bad28f3347dd0216ea8e27977f7af33f626f912",
-  stream10: "sha256:798fbe5c40fba2609fae716f5414af6654df21d2552927e85de6a11d20bbff7d",
+  stream9:
+    "sha256:0cae7e53f997228f0afc523f1bad28f3347dd0216ea8e27977f7af33f626f912",
+  stream10:
+    "sha256:798fbe5c40fba2609fae716f5414af6654df21d2552927e85de6a11d20bbff7d",
 } as const;
