ef284178d44c5b8de2b79f567eb417137b251d3a
adrobuta <alexandra.drobut@snyk.io>
2025-12-18 17:36:18 +0200

    feat: support OCI images without platform tags
    - Upgrade @snyk/docker-registry-v2-client from 2.23.0 to 2.24.0
    - Add test for snykgoof/oci-goof:ociNoPlatformTag

--- test/system/image-type/identifier.spec.ts
+++ test/system/image-type/identifier.spec.ts
@@ -1,4 +1,5 @@
 import { scan } from "../../../lib/index";
+import { execute } from "../../../lib/sub-process";
 
 describe("handles bad input being provided", () => {
   it("should reject when provided with a non-existent image and tag", async () => {
@@ -7,5 +8,32 @@ describe("handles bad input being provided", () => {
         path: "not-here:latest",
       }),
     ).rejects.toEqual(Error("authentication required"));
+  });
+});
+
+describe("OCI images without platform tags", () => {
+  afterAll(async () => {
+    await execute("docker", [
+      "image",
+      "rm",
+      "snykgoof/oci-goof:ociNoPlatformTag",
+    ]).catch(() => {
+      console.error(`tests teardown failed to remove docker image`);
+    });
+  });
+
+  it("should correctly scan an OCI image without platform information in the tag", async () => {
+    const image = "snykgoof/oci-goof:ociNoPlatformTag";
+
+    const pluginResult = await scan({
+      path: image,
+      username: process.env.DOCKER_HUB_USERNAME,
+      password: process.env.DOCKER_HUB_PASSWORD,
+    });
+
+    expect(pluginResult.scanResults).toBeDefined();
+    expect(pluginResult.scanResults.length).toBeGreaterThan(0);
+    expect(pluginResult.scanResults[0].facts).toBeDefined();
+    expect(pluginResult.scanResults[0].facts.length).toBeGreaterThan(0);
   });
 });
