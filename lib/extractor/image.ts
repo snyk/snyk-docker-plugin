694b9a655c6d889db8f30f2e8ba9cd9b7dfa074d
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-02-19 16:22:26 -0500

    fix: use central parser in imagename

--- lib/extractor/image.ts
+++ lib/extractor/image.ts
@@ -1,3 +1,4 @@
+import { parseImageReference } from "../image-reference";
 import { PluginOptions } from "../types";
 
 export { ImageName, ImageDigest, getImageNames };
@@ -10,50 +11,12 @@ class ImageName {
     targetImage: string,
     digests: { manifest?: string; index?: string } = {},
   ) {
-    // this regex has been copied from
-    // https://github.com/distribution/distribution/blob/fb2188868d771aa27e5781a32bf78d4c113c18a6/reference/regexp.go#L101
-    // (code has been modified to print the regex), and then adjusted to
-    // Javascript. The required modifications were replacing `[:xdigit:]` with
-    // `[a-fA-F0-9]` and escaping the slashes.
-    // Note that the digest matched in this Regex will match digests that have
-    // uppercase-letters, while the regex used in validateDigest does NOT match
-    // uppercase-letters. This simply matches the behaviour from the upstream
-    // `reference` and `go-digest `packages.
-    //
-    // we're matching pattern: <registry:port_number>(optional)/<image_name>(mandatory):<image_tag>(optional)@<tag_identifier>(optional)
-    // This Regex contains three capture groups:
-    // 1) The repository / image name
-    // 2) tag
-    // 3) digest
-    const re =
-      /^((?:(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])(?:(?:\.(?:[a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]))+)?(?::[0-9]+)?\/)?[a-z0-9]+(?:(?:(?:[._]|__|[-]*)[a-z0-9]+)+)?(?:(?:\/[a-z0-9]+(?:(?:(?:[._]|__|[-]*)[a-z0-9]+)+)?)+)?)(?::([\w][\w.-]{0,127}))?(?:@([A-Za-z][A-Za-z0-9]*(?:[-_+.][A-Za-z][A-Za-z0-9]*)*[:][A-Fa-f0-9]{32,}))?$/gi;
+    const parsed = parseImageReference(targetImage);
+    this.name = parsed.registry ? parsed.registry + "/" + parsed.repository : parsed.repository;
 
-    const groups = re.exec(targetImage);
-    if (groups === null) {
-      if (targetImage === "") {
-        throw new Error("image name is empty");
-      }
-      if (re.exec(targetImage.toLowerCase()) !== null) {
-        throw new Error("image repository contains uppercase letter");
-      }
-      throw new Error("invalid image reference format");
-    }
-
-    const parsedGroups = {
-      name: groups[1],
-      tag: groups[2],
-      digest: groups[3],
-    };
-
-    this.name = parsedGroups.name;
-
-    const NameTotalLengthMax = 255;
-    if (this.name.length > NameTotalLengthMax) {
-      throw new Error("image repository name is more than 255 characters");
-    }
-
-    this.tag =
-      parsedGroups.tag || parsedGroups.digest ? parsedGroups.tag : "latest";
+    // If the image name has a tag, use it. If the image name has
+    // nether a tag nor a digest, use "latest".
+    this.tag = parsed.tag || parsed.digest ? parsed.tag : "latest";
 
     this.digests = {};
     if (digests.index) {
@@ -63,11 +26,9 @@ class ImageName {
       this.digests.manifest = new ImageDigest(digests.manifest);
     }
 
-    if (parsedGroups.digest) {
-      const digest = new ImageDigest(parsedGroups.digest);
-      if (this.digests.manifest !== digest && this.digests.index !== digest) {
-        this.digests.unknown = digest;
-      }
+    // If the image name has a digest, and it's not the same as the manifest or index digest, add it as the unknown digest.
+    if (parsed.digest && !this.digests.manifest?.equals(parsed.digest) && !this.digests.index?.equals(parsed.digest)) {
+        this.digests.unknown = new ImageDigest(parsed.digest);
     }
   }
 
@@ -130,6 +91,13 @@ class ImageDigest {
 
   public toString(): string {
     return this.alg + ":" + this.hex;
+  }
+
+  public equals(other: ImageDigest | string): boolean {
+    if (typeof other === "string") {
+      return this.toString() === other;
+    }
+    return this.alg === other.alg && this.hex === other.hex;
   }
 }
 
