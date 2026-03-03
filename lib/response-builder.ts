e409373a4a89e923b4b225169bfd8e100bb70f0c
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-03-03 11:47:42 -0500

    fix: restore dependency filtering

--- lib/response-builder.ts
+++ lib/response-builder.ts
@@ -37,6 +37,9 @@ async function buildResponse(
     excludeBaseImageVulns,
   );
 
+  // Apply the filtered dependencies back to the depTree
+  depsAnalysis.depTree.dependencies = finalDeps;
+
   /** This must be called after all final changes to the DependencyTree. */
   const depGraph = await legacy.depTreeToGraph(
     depsAnalysis.depTree,
