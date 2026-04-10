bdeddc74352a6e8752863acd5d2d4c152469ffa9
Parker Kuivila <parker.kuivila@snyk.io>
2026-04-10 09:18:06 -0400

    refactor: replace mkdirp, uuid, tmp with node builtins (#779)
    - Replace fs.rmdirSync with fs.rmSync({ recursive, force }) in system
      tests to handle non-empty temp dirs left by getImageArchive creating
      subdirs before a failed pull
    - Extend .snyk ignore expiry for SNYK-JS-TAR-15307072/15416075/15456201
      (transitive via snyk-nodejs-lockfile-parser > @yarnpkg/core > tar)
      from 2026-04-03 to 2026-07-06

--- lib/image-save-path.ts
+++ lib/image-save-path.ts
@@ -1,12 +1,12 @@
+import * as crypto from "crypto";
+import * as os from "os";
 import * as path from "path";
-import * as tmp from "tmp";
-import { v4 as uuidv4 } from "uuid";
 
 export function fullImageSavePath(imageSavePath: string | undefined): string {
-  let imagePath = tmp.dirSync().name;
+  let imagePath = os.tmpdir();
   if (imageSavePath) {
     imagePath = path.normalize(imageSavePath);
   }
 
-  return path.join(imagePath, uuidv4());
+  return path.join(imagePath, crypto.randomUUID());
 }
