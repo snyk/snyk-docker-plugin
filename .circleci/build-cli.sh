56695fbaf6ab9406ca9b01f4c69cd1b49597efcc
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-06-11 13:28:49 -0400

    chore: update npm version in build-cli.sh to satisfy engine requirement
    Snyk CLI now requires npm ^11.10; the script was pinning npm@7 which caused EBADENGINE failures.

--- .circleci/build-cli.sh
+++ .circleci/build-cli.sh
@@ -6,7 +6,7 @@ git clone https://github.com/snyk/cli.git
 cd cli
 echo "Replacing snyk-docker-plugin dependency with SHA $CIRCLE_SHA1"
 sed -iE  "s/\"snyk-docker-plugin\": \".*\",/\"snyk-docker-plugin\": \"git:\/\/github.com\/snyk\/snyk-docker-plugin.git#$CIRCLE_SHA1\",/" package.json
-sudo npm i -g npm@7
+sudo npm i -g npm@11
 echo "Running npm install"
 npm i
 echo "Running build"
