c87faf71553a8a0dfdb418ec23ef899170aed068
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-06-17 13:19:45 -0400

    chore(ci): remove outdated snapshot configs

--- jest.config.js
+++ jest.config.js
@@ -8,16 +8,6 @@ module.exports = {
   testMatch: ["<rootDir>/test/**/*.spec.ts"],
   testPathIgnorePatterns: ["<rootDir>/test/windows/"],
   testTimeout: 600000, // 10 minutes
-  // TODO: This option is printing Array\Object prefixing arrays\objects in snapshots files
-  // this settings were added when migrated to Jest 29 to support the old snapshots format
-  // this settings should be removed in the future after snapshot migration to the new Jest snapshot format
-  // more details over the breaking change of Jest snapshot format can be found here:
-  // https://jestjs.io/docs/next/upgrading-to-jest29#snapshot-format
-  snapshotFormat: {
-    escapeString: true,
-    printBasicPrototype: true,
-  },
-
   // TODO: This is here until a bug in Jest (which in turn affects ts-jest) is resolved.
   // It affects our CI/CD runs and makes the machine run out of memory.
   // https://github.com/facebook/jest/issues/10550
