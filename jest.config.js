0af57e54e22e1cc4bc0d1db4debee06a5f86edd9
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-06-18 09:56:41 -0400

    chore(test): centralize Jest config using projects
    Consolidate unit, system, and windows test suites into a single
    jest.config.js using Jest projects. Removes test/windows/jest*.config.js
    and replaces --testPathPattern/--config flags with --selectProjects.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
-module.exports = {
+const shared = {
   preset: "ts-jest",
   setupFilesAfterEnv: [
     "<rootDir>/test/jest-snapshot-strip-analytics.cjs",
     "<rootDir>/test/matchers/setup.ts",
   ],
   testEnvironment: "node",
-  testMatch: ["<rootDir>/test/**/*.spec.ts"],
-  testPathIgnorePatterns: ["<rootDir>/test/windows/"],
   testTimeout: 600000, // 10 minutes
   // TODO: This is here until a bug in Jest (which in turn affects ts-jest) is resolved.
   // It affects our CI/CD runs and makes the machine run out of memory.
@@ -20,8 +18,44 @@ module.exports = {
       },
     ],
   },
+  snapshotFormat: {
+    escapeString: true,
+    printBasicPrototype: true,
+  },
+};
+
+module.exports = {
   // Use 3 of 4 CircleCI cores
   // https://github.com/jestjs/jest/issues/11956#issuecomment-1212925677
   maxWorkers: 3,
   reporters: ["default", ["jest-junit", { outputDirectory: "test/reports" }]],
+  projects: [
+    {
+      ...shared,
+      displayName: "unit",
+      testMatch: ["<rootDir>/test/(lib|unit)/**/*.spec.ts"],
+    },
+    {
+      ...shared,
+      displayName: "system",
+      testMatch: ["<rootDir>/test/system/**/*.spec.ts"],
+    },
+    {
+      ...shared,
+      displayName: "windows",
+      testMatch: ["<rootDir>/test/windows/**/*.spec.ts"],
+      testPathIgnorePatterns: [
+        "<rootDir>/test/windows/lib/image-inspector.spec.ts",
+        "<rootDir>/test/windows/registry-scan.spec.ts",
+      ],
+    },
+    {
+      ...shared,
+      displayName: "windows:docker",
+      testMatch: [
+        "<rootDir>/test/windows/lib/image-inspector.spec.ts",
+        "<rootDir>/test/windows/registry-scan.spec.ts",
+      ],
+    },
+  ],
 };
