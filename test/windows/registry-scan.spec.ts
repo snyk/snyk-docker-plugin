084568156d964e32de127b4d917265b27d78d849
Devon Colmer <935806+d3vco@users.noreply.github.com>
2026-07-08 16:29:52 -0400

    chore: skip windows registry pull test
    The latest CircleCI windows executor is no longer able to pull linux
    docker images. We could pull a windows image, but there is little value
    in the scan output. Skipping the test while we determine a path forward.

--- test/windows/registry-scan.spec.ts
+++ test/windows/registry-scan.spec.ts
@@ -3,7 +3,7 @@ import { DepGraph } from "@snyk/dep-graph";
 import * as plugin from "../../lib";
 
 describe("windows scanning", () => {
-  it("can scan a registry image (alpine:3.12.0)", async () => {
+  test.skip("can scan a registry image (alpine:3.12.0)", async () => {
     const pluginResult = await plugin.scan({
       path: "alpine@sha256:185518070891758909c9f839cf4ca393ee977ac378609f700f60a771a2dfe321",
       platform: "linux/amd64",
@@ -13,6 +13,7 @@ describe("windows scanning", () => {
     const depGraph: DepGraph = pluginResult.scanResults[0].facts.find(
       (fact) => fact.type === "depGraph",
     )!.data;
+
     expect(depGraph.rootPkg.name).toEqual("docker-image|alpine");
     expect(depGraph.rootPkg.version).toBeUndefined();
     expect(pluginResult.scanResults[0].identity.type).toEqual("apk");
